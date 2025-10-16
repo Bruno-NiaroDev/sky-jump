caec8a5cbe4c4126b752b21a5dfd3c2436bba4cc
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2025-10-15 22:18:29 -0300

    chore(killzone): Adicionado funcao base da killzone que será reutilizado em todas as vezes.

---
+++ scripts/killzone.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You died!")
+	Engine.time_scale = 0.5
+	## TO DO - Definir collision do player com esse nome: "CollisionPlayer"
+	body.get_node("CollisionPlayer").queue_free()
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	print("Restarting level")
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
