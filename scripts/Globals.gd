f12a87453840d8ea1b163d845f24a932d0990418
Luiz-1213 <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-10-27 16:39:32 -0300

    feat(HUD): implementando o menu HUD

---
+++ scripts/Globals.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var crystals := 0
+var player_life := 3
+
+var player = null
+var current_checkpoint = null
+var player_start_position = null
+
+
+func respawn_player():
+	if current_checkpoint!= null:
+		player.position = current_checkpoint.global_position
+	else:
+		player.global_position = player_start_position.global_position
