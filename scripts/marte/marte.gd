12705297d91f380bd2083cd068955eb23475412d
LiviaVenezian1 <livia.venezian@sou.unifeob.edu.br>
2025-10-29 21:56:54 -0300

    feat(marte) adicionando player

---
+++ scripts/marte/marte.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+@onready var player := $player as CharacterBody2D
+@onready var player_scene = preload("res://scenes/common/characters/player.tscn")
+@onready var camera := $camera as Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	Globals.player = player
+	player.follow_camera(camera)
+	Globals.player.player_has_died.connect(reload_game)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func reload_game():
+	await get_tree().create_timer(1.0).timeout
+	var player = player_scene.instantiate()
+	add_child(player)
+	Globals.player = player
+	player.follow_camera(camera)
+	Globals.player.player_has_died.connect(game_over)
+	Globals.elements = 0
+	Globals.player_life = 3
+	Globals.respawn_player()
+	
+func game_over():
+	print('você morreu')
+	#get_tree().change_scene_to_file("res://scenes/game_over_screen.tscn")
