7c9f597a901e149beeaa185c6cca53582cd71fcc
Luiz-1213 <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-11-03 16:36:01 -0300

    feat(screens): add input name

--- scripts/common/menu.gd
+++ scripts/common/menu.gd
@@ -1,18 +1,20 @@
 extends Control
 
+@onready var buttons := $buttons
+@onready var form := $form
+@onready var name_input := $form/input
+@onready var error_label := $form/error_label
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	buttons.visible = true
+	form.visible = false
+	error_label.visible = false
 
 
 func _on_start_btn_pressed() -> void:
-	get_tree().change_scene_to_file('res://screens/mapa.tscn')
+	buttons.visible = false
+	form.visible = true
+	
 
 
 func _on_quit_btn_pressed() -> void:
@@ -25,3 +27,12 @@ func _on_load_btn_pressed() -> void:
 
 func _on_ranking_btn_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_confirm_btn_pressed() -> void:
+	var nome = name_input.text.strip_edges()
+	if nome == "":
+		error_label.visible = true
+	else:
+		error_label.visible = false
+		get_tree().change_scene_to_file('res://screens/mapa.tscn')
