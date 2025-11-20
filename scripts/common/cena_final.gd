ecb010c92d919f0022b45bef126603650ed83cd6
Luiz-1213 <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-11-20 16:42:15 -0300

    feat(terra):add final scene

--- scripts/common/cena_final.gd
+++ scripts/common/cena_final.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_video_stream_player_finished() -> void:
-	get_tree().change_scene_to_file("res://screens/ranking.tscn")
+	get_tree().change_scene_to_file("res://screens/cena_familia.tscn")
