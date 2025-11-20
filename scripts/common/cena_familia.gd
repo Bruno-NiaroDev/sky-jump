89f3ae094e220b12f58f1c4bc6c86efc8d2c1133
LiviaVenezian1 <livia.venezian@sou.unifeob.edu.br>
2025-11-20 20:34:11 -0300

    add som final

--- scripts/common/cena_familia.gd
+++ scripts/common/cena_familia.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var fim_sfx: AudioStreamPlayer = $fim_sfx
 
 func _on_next_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://screens/ranking.tscn")
--- scripts/common/cena_final.gd
+++ scripts/common/cena_final.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var ultimo_sfx: AudioStreamPlayer = $ultimo_sfx
 
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://screens/cena_familia.tscn")
